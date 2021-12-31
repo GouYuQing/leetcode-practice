b79832de16280006d1c0e2c58b3585c98aba030a
gouyuqing <gouyuqing@everhigh.com.cn>
2021-12-31 19:02:12 +0800

    三数之和

--- 三数之和.js
+++ 三数之和.js
@@ -1,28 +1,28 @@
 /**
- * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？请你找出所有和为 0 且不重复的三元组。
+ * 给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + third + second = 0 ？请你找出所有和为 0 且不重复的三元组。
  */
-var threeSum = function (nums) { 
-	if (nums.length < 3) { 
-		return [];
-	}
-	nums.sort((a, b) => { return a - b; });
-	const res = [];
-	for (let i = 0; i < nums.length; i++) { 
-		if (nums[i] > 0) break;
+var threeSum = function (nums) {
+	let n = nums.length;
+	nums.sort((a, b) => a - b);
+	let res = [];
+	for (let i = 0; i < n - 2; i++) {
+		if (nums[i] === nums[i - 1]) continue;
 		let first = nums[i];
-		if (i > 0 && nums[i] == nums[i - 1]) continue;
-		let new_arr = new Set();
-		for (let j = i + 1; j < nums.length; j++) { 
+		let map = new Map();
+		for (let j = i + 1; j < n; j++) {
 			let third = nums[j];
-			let second = -(first + third);
-			if (new_arr.has(second)) {
-				res.push([first, third, -(first + third)]);
-				while (j < nums.length - 1 && nums[j] == nums[j + 1]) j++;
+			let second = -first - third;
+			if (res.length) {
+				let [x, y, z] = res[res.length - 1];
+				if (y === third && z === second) continue;
 			}
-			new_arr.add(third);
+			if (map.has(second)) {
+				res.push([first, third, second]);
+			} else {
+				map.set(third, 1);
+			}
 		}
 	}
-	
 	return res;
-}
+};
 console.log(threeSum([-1, 0, 1, 2, -1, -4]));
