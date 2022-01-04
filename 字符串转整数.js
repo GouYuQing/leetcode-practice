ee4f81da29551b0f93e9b7220bb8c64f68f9356d
gouyuqing <gouyuqing@everhigh.com.cn>
2022-01-04 11:20:11 +0800

    字符串转整数

---
+++ 字符串转整数.js
@@ -0,0 +1,16 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+ var myAtoi = function(s) {
+    let res = parseInt(s.trim());
+    if(res > Math.pow(2,31) - 1){
+        return Math.pow(2,31) - 1;
+    }
+    if(res < -Math.pow(2, 31)){
+        return -Math.pow(2, 31);
+    }
+    if(isNaN(res)) return 0;
+    return res;
+};
+console.log(myAtoi("words and 987"));
