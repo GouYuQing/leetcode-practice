388c5b2128906e57150022d40df90a42e2d22d20
GouYuQing <3319948815@qq.com>
2022-01-12 23:19:19 +0800

    两数相加

---
+++ 两数相加.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function (l1, l2) {
+	let add = 0; // 有进位
+	let sum = new ListNode('0');
+	let head = sum;
+	while (l1 || l2 || add) {
+		let val1 = l1 !== null ? l1.val : 0;
+		let val2 = l2 !== null ? l2.val : 0;
+		let temp_res = val1 + val2 + add;
+		add = temp_res >= 10 ? 1 : 0;
+		sum.next = new ListNode(temp_res % 10); //sum的下一个节点
+		sum = sum.next;
+		if (l1) l1 = l1.next;
+		if (l2) l2 = l2.next;
+	}
+	return head.next;
+};
