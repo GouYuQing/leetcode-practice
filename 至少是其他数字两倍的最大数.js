deb0a666a2b892da01dd9d2a83f62e88272fe0d2
GouYuQing <3319948815@qq.com>
2022-01-13 23:07:06 +0800

    至少是其他数字两倍的最大数

---
+++ 至少是其他数字两倍的最大数.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var dominantIndex = function (nums) {
+	let index = null;
+	if (nums.length === 1) return 0;
+	let arr = [...new Set(nums.map(item => item))];
+	arr.sort((a, b) => a - b);
+	let max_num = arr[arr.length - 1],
+		max_second = arr[arr.length - 2];
+	console.log(max_num, max_second, nums);
+	if (max_num / max_second >= 2) {
+		index = nums.indexOf(max_num);
+	} else {
+		index = -1;
+	}
+	return index;
+};
+console.log(dominantIndex([1]));
