e3a90d3e59699720c324853e11db5b5576140128
GouYuQing <3319948815@qq.com>
2022-01-23 19:35:36 +0800

    路径总和

---
+++ 深度优先遍历和递归/路径总和.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} targetSum
+ * @return {boolean}
+ */
+var hasPathSum = function (root, targetSum) {
+	function findSum(node, sum) {
+		if (node == null) return false;
+		sum += node.val;
+		if (sum == targetSum && node.left == null && node.right == null) return true;
+		return findSum(node.left) || findSum(node.right);
+	}
+	return findSum(root, 0);
+};
