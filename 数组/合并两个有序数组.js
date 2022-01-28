002150922c0538c10afa9ab60b0d8e67e67b255c
gouyuqing <gouyuqing@everhigh.com.cn>
2022-01-28 15:42:07 +0800

    合并两个有序数组

---
+++ 数组/合并两个有序数组.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ *
+ * 请你 合并 nums2 到 nums1 中，使合并后的数组同样按 非递减顺序 排列。
+ * 注意：最终，合并后数组不应由函数返回，而是存储在数组 nums1 中。为了应对这种情况，nums1 的初始长度为 m + n，其中前 m 个元素表示应合并的元素，后 n 个元素为 0 ，应忽略。nums2 的长度为 n 。
+ */
+var merge = function (nums1, m, nums2, n) {
+	for (let i = m, j = 0; i < m + n, j < n; i++, j++) {
+		nums1[i] = nums2[j];
+	}
+	return nums1.sort((a, b) => a - b);
+};
